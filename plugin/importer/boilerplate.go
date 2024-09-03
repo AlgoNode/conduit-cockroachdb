55007e5638ae4581ba42a7446137a4a04231b34c
Agustin Godnic <agustingodnic@gmail.com>
2024-09-03 18:44:41 -0300

    Update config

--- plugin/importer/boilerplate.go
+++ plugin/importer/boilerplate.go
@@ -8,22 +8,21 @@ import (
 	"github.com/algorand/go-algorand-sdk/v2/types"
 	"github.com/labstack/gommon/log"
 	"github.com/sirupsen/logrus"
-	"gopkg.in/yaml.v2"
 
 	"github.com/algorand/conduit/conduit/data"
 	"github.com/algorand/conduit/conduit/plugins"
 	"github.com/algorand/conduit/conduit/plugins/importers"
 )
 
-////go:embed sample.yaml
-//var sampleConfig string
+//go:embed sample.yaml
+var sampleConfig string
 
 // metadata contains information about the plugin used for CLI helpers.
 var metadata = plugins.Metadata{
-	Name:        "ndlycloud",
-	Description: "Example importer.",
-	Deprecated:  false,
-	//SampleConfig: sampleConfig,
+	Name:         "ndlycloud",
+	Description:  "Importer that fetches blocks concurrently from an algod v2 REST API.",
+	Deprecated:   false,
+	SampleConfig: sampleConfig,
 }
 
 func init() {
@@ -34,8 +33,9 @@ func init() {
 
 type Config struct {
 	// TODO: your configuration here.
-	ConfigString string `yaml:"config_string"`
-	ConfigInt    int    `yaml:"config_int"`
+	NetAddr string `yaml:"netaddr"`
+	Token   string `yaml:"token"`
+	Workers uint64 `yaml:"workers"`
 }
 
 // importerPlugin is the object which implements the importerPlugin plugin interface.
@@ -49,23 +49,20 @@ func (it *importerPlugin) Metadata() plugins.Metadata {
 	return metadata
 }
 
-func (it *importerPlugin) Config() string {
-	ret, _ := yaml.Marshal(it.cfg)
-	return string(ret)
-}
-
 func (it *importerPlugin) Close() error {
 	it.wp.close()
 	return nil
 }
 
 func (it *importerPlugin) Init(ctx context.Context, initProvider data.InitProvider, cfg plugins.PluginConfig, logger *logrus.Logger) error {
+
 	it.log = logger
 	var err error
 	if err = cfg.UnmarshalConfig(&it.cfg); err != nil {
 		return fmt.Errorf("unable to read configuration: %w", err)
 	}
-	it.wp, err = newWorkerPool(ctx, logger, 3, uint64(initProvider.NextDBRound())) //FIXME hardcoded value
+	logger.Infof("CONFIG netaddr=%s workers=%d", it.cfg.NetAddr, it.cfg.Workers)
+	it.wp, err = newWorkerPool(ctx, logger, it.cfg.Workers, uint64(initProvider.NextDBRound()))
 	if err != nil {
 		return fmt.Errorf("failed to initialize worker pool: %w", err)
 	}
