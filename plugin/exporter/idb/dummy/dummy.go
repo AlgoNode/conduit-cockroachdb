82e418beab5c3f72313c6934ecc2fb7ab3487674
Agustin Godnic <agustingodnic@gmail.com>
2025-09-10 19:34:32 -0300

    Update broken import

--- plugin/exporter/idb/dummy/dummy.go
+++ plugin/exporter/idb/dummy/dummy.go
@@ -5,7 +5,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
-	"github.com/algonode/conduit-cockroachdb/plugin/exporter/idb"
+	"github.com/algorand/indexer/v3/idb"
 	"github.com/algorand/indexer/v3/types"
 
 	sdk "github.com/algorand/go-algorand-sdk/v2/types"
